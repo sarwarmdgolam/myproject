5272bfcabde6c371bcf295556e9b2edf7000d449
golam sarwar <mdgolam.sarwar@bracits.com>
2023-02-19 17:02:13 +0600

    test added

--- adminapp/tests.py
+++ adminapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+from adminapp.models import Job
+
+
 # Create your tests here.
+
+
+class JobTests(TestCase):
+    def test_job_is_created_successfully(self):
+        job = Job(
+            name='test_job',
+        )
+        job.save()
+
+
